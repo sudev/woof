73de13c3cb0b7eba9650c866357d0dba3bd6fe3d
Rohith Uppala <rohith.uppala@goibibo.com>
2016-03-05 15:43:49 +0530

    test file for woof transactions

---
+++ transactions_test.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import logging
+import time
+from woof.transactions import TransactionLogger
+
+stime = time.time()
+tr = TransactionLogger("nmlgoibibofe65:9092", "transaction_dummy")
+print "Time taken for connection: ", time.time() - stime
+
+stime = time.time()
+tr.New("gofld3434", 3500, "htld864578346578", "{'foo':'bar'}", "rohith2506", "r1@gmail.com", "8984758345345")
+print "Time taken to send one message: ", time.time() - stime
--- woof/transactions.py
+++ woof/transactions.py
@@ -1,6 +1,5 @@
 from woof.partitioned_producer import PartitionedProducer
 
-
 class TransactionLogger():
     def __init__(self, broker, vertical, ):
         self.broker = broker
@@ -9,28 +8,27 @@ class TransactionLogger():
         self.topic = _get_topic_from_vertical(vertical)
 
     def New(self, txn_id, amount, skus, detail="#", userid="#", email="#", phone="#"):
-        self._send_log("NEW", verb, txn_id, amount, skus, detail, userid, email, phone)
+        self._send_log("NEW", txn_id, amount, skus, detail, userid, email, phone)
 
     def Modify(self, txn_id, amount, skus, detail="#", userid="#", email="#", phone="#"):
-        self._send_log("MODIFY", verb, txn_id, amount, skus, detail, userid, email, phone)
+        self._send_log("MODIFY", txn_id, amount, skus, detail, userid, email, phone)
 
     def Cancel(self, txn_id, amount, skus, detail="#", userid="#", email="#", phone="#"):
-        self._send_log("CANCEL", verb, txn_id, amount, skus, detail, userid, email, phone)
+        self._send_log("CANCEL", txn_id, amount, skus, detail, userid, email, phone)
 
     def Fulfil(self, txn_id, amount, skus, detail="#", userid="#", email="#", phone="#"):
-        self._send_log("FULFIL", verb, txn_id, amount, skus, detail, userid, email, phone)
+        self._send_log("FULFIL", txn_id, amount, skus, detail, userid, email, phone)
 
     def _send_log(self, verb, txn_id, amount, skus, detail="#", userid="#", email="#", phone="#"):
-        msg = self._format_message(self, verb, txn_id, amount, skus, detail, userid, email, phone)
+        msg = self._format_message(verb, txn_id, amount, skus, detail, userid, email, phone)
         self.producer.send(self.topic, txn_id, msg)
 
     def _format_message(self, verb, txn_id, amount, skus, detail, userid, email, phone):
         """
         Generates log message.
         """
-
         separator = '\t\x01'
-        return separator.join(verb, txn_id, amount, skus, detail, userid, email, phone)
+        return separator.join([verb, txn_id, str(amount), skus, detail, userid, email, phone])
 
 
 def _get_topic_from_vertical(topic):
