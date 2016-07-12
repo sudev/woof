78b65e9f7c79161387e7d4ef77b6191755fbaf7a
Sudev Ambadi <sudev.ac@.goibibo.com>
2016-07-12 16:52:26 +0530

    Some nodes not to be present

--- scripts/zk_to_kafka.py
+++ scripts/zk_to_kafka.py
@@ -10,13 +10,19 @@ ZK_OFFSETS_PATH_FMT = "/consumers/%s/offsets/"
 def get_offsets(zk, group, topic):
     path = ZK_OFFSETS_PATH_FMT % (group)
     path += topic
-    partitions = zk.get_children(path)
+    try:
+        partitions = zk.get_children(path)
+    except:
+        print "Check if consumer group name is correct"
 
     offsets_topic = {}
 
     for part in partitions:
         path += "/%s" % part
-        offset, meta = zk.get(path)
+        try:
+            offset, meta = zk.get(path)
+        except:
+            offset = 0
         offsets_topic[part] = offset
 
     print "Offsets for topic ", topic
@@ -28,28 +34,29 @@ def get_offsets(zk, group, topic):
 def commit_offsets_in_kafka(cons, topic, offsets_topic):
 
     for partition, offset in offsets_topic.items():
-        tp = TopicPartition(topic,int(partition))
+        tp = TopicPartition(topic, int(partition))
         cons.assign([tp])
         cons.seek(tp, int(offset))
         # commit it
         cons.commit()
 
 
-
-
-
 if __name__ == "__main__":
     logging.basicConfig(
-            format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
-            filename='/tmp/woofload',
-            level=logging.INFO
-    )
+        format=
+        '%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
+        filename='/tmp/woofload',
+        level=logging.INFO)
 
     parser = argparse.ArgumentParser()
     parser.add_argument("zk_server", help="host:port to the zookeeper server")
-    parser.add_argument("kafka_broker", help="host:port to the kafka broker server")
+    parser.add_argument("kafka_broker",
+                        help="host:port to the kafka broker server")
     parser.add_argument("group", help="consumer group name ")
-    parser.add_argument("-t", "--topic", default="all", help="topic to use, all for all topics")
+    parser.add_argument("-t",
+                        "--topic",
+                        default="all",
+                        help="topic to use, all for all topics")
     args = parser.parse_args()
 
     # TODO
@@ -58,9 +65,13 @@ if __name__ == "__main__":
     zk = KazooClient(hosts=args.zk_server)
     zk.start()
 
-    kafka_cons = KafkaConsumer(bootstrap_servers=args.kafka_broker, group_id = args.group)
+    kafka_cons = KafkaConsumer(bootstrap_servers=args.kafka_broker,
+                               group_id=args.group)
 
     # get offsets from Zk
     offsets_topic = get_offsets(zk, args.group, args.topic)
     #commit them to Kafka
-    commit_offsets_in_kafka(kafka_cons, args.topic,offsets_topic)
+    try:
+        commit_offsets_in_kafka(kafka_cons, args.topic, offsets_topic)
+    except:
+        print "Kafka Storage offset commit failed."
